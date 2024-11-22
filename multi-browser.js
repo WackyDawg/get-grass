035e8279155c2209b527376c0cd198926ebeb84a
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-11-22 11:55:52 -0800

    Update multi-browser.js

--- multi-browser.js
+++ multi-browser.js
@@ -34,7 +34,7 @@ puppeteer.use(StealthPlugin());
             const randomUserAgent = shuffledUserAgents.pop(); 
 
             const browser = await puppeteer.launch({
-                headless: false,
+                headless: true,
                 args: [
                     '--no-sandbox', 
                     '--disable-setuid-sandbox', 
